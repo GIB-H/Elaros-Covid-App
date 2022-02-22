15ffcff36dcb78b6cfb60fbdd971e276aab5beb2
George <72026233+GIB-H@users.noreply.github.com>
2022-02-22 17:53:30 +0000

    added button to navigate back from another screen

--- screens/review.js
+++ screens/review.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button} from "react-native";
 import { globalStyles } from "../styles/global";
 
-export default function Review() {
+export default function Review({ navigation }) {
+
+    const pressHandler = () => {
+        navigation.goBack();
+    }
+
     return(
         <View style={globalStyles.container}>
             <Text style={globalStyles.titleText}>Review Screen</Text>
+            <Button title="Back to home screen" onPress={pressHandler} />
         </View>
     )
 }
